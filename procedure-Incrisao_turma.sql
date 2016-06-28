3e05301bd9b3f79ab406758ee0b16d7f10888853
ThiagoYonamine <thyonamine1@gmail.com>
2016-06-27 23:57:37 -0300

    procedure inscreve aluno em uma turma de uma materia, ou altera uma turma

--- procedure-Incrisao_turma.sql
+++ procedure-Incrisao_turma.sql
@@ -1,12 +1,29 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `inscricao_turma`(n_cpf char(12), n_id_turma int, n_fase int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inscricao_turma`(n_cpf char(12), n_id_turma int, n_fase int, n_op int, n_siglaDisciplina char(10))
 BEGIN
 DECLARE  CpfAluno CHAR(12);
+DECLARE  Disciplina CHAR(10);
+#verifica se cpf é de um aluno
 select labbd.aluno.cpf into CpfAluno
 from labbd.aluno
 where labbd.aluno.cpf = n_cpf;
 IF (CpfAluno = n_cpf) THEN
+	
+	if(n_op = 1) then
 	insert into labbd.matricula(cpf, id_turma, fase)
 	values (n_cpf, n_id_turma, n_fase);
-
+	end if;
+    
+    if(n_op = 2) then
+    select  turma.sigla into Disciplina
+	from turma
+	where turma.id_turma = n_id_turma;
+    
+    update labbd.matricula
+	set cpf = n_cpf,
+    id_turma = n_id_turma,
+    fase = n_fase
+    WHERE matricula.cpf = CpfAluno and  Disciplina = n_siglaDisciplina;
+	
+    end if;
 END IF;
 END
