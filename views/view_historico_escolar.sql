c564f78b43d714b54b42e2c368bfae2c57123ed4
dcardos <cardoso.nilo@gmail.com>
2016-07-01 15:27:32 -0300

    organizando

--- views/view_historico_escolar.sql
+++ views/view_historico_escolar.sql
@@ -1,6 +1,7 @@
 CREATE VIEW vhistorico AS
-	SELECT turma.letra AS 'Turma', disciplina.nome, disciplina.creditos, matricula.nota, matricula.deferimento AS 'Resultado'
+	SELECT turma.letra AS 'Turma', disciplina.nome, disciplina.creditos, matricula.nota, 
+			matricula.frequencia, matricula.deferimento AS 'Resultado'
 	FROM disciplina INNER JOIN turma ON turma.sigla = disciplina.sigla
 					INNER JOIN matricula ON matricula.id_turma = turma.id_turma
-					INNER JOIN aluno ON aluno.cpf = matricula.cpf
-	WHERE aluno.cpf = '919136532-44';
+					INNER JOIN aluno ON aluno.cpf = matricula.cpf;
+	#WHERE aluno.cpf = '919136532-44';
