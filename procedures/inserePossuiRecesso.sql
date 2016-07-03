7fb318428f9ecde693bc03a796cfc9149c883b22
bbortolli <bbortolli2@hotmail.com>
2016-07-03 18:32:40 -0300

    Correção
    calendarios EAD e Presencial tem ID's diferentes, antes não tratados corretamente. nesse commit isso foi arrumado

--- procedures/inserePossuiRecesso.sql
+++ procedures/inserePossuiRecesso.sql
@@ -1,12 +1,12 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `inserePossuiRecesso`(n_id_recesso INT, n_id_calendario INT, tipo INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `inserePossuiRecesso`(n_id_recesso INT, n_id_calendario_p INT, n_id_calendario_e INT, tipo INT)
 BEGIN
 	IF (tipo = 0) THEN
-		insert into labbd.possui_recesso_ead(id_recesso, id_ead) values (n_id_recesso, n_id_calendario);
-		insert into labbd.possui_recesso_presencial(id_recesso, id_presencial) values (n_id_recesso, n_id_calendario);
+		insert into labbd.possui_recesso_ead(id_recesso, id_ead) values (n_id_recesso, n_id_calendario_e);
+		insert into labbd.possui_recesso_presencial(id_recesso, id_presencial) values (n_id_recesso, n_id_calendario_p);
 	ELSEIF (tipo = 1) THEN
-		insert into labbd.possui_recesso_presencial(id_recesso, id_presencial) values (n_id_recesso, n_id_calendario);
+		insert into labbd.possui_recesso_presencial(id_recesso, id_presencial) values (n_id_recesso, n_id_calendario_p);
 	ELSEIF (tipo = 2) THEN
-		insert into labbd.possui_recesso_ead(id_recesso, id_ead) values (n_id_recesso, n_id_calendario);
+		insert into labbd.possui_recesso_ead(id_recesso, id_ead) values (n_id_recesso, n_id_calendario_e);
 	ELSE
 		CALL raise_error();
 			
