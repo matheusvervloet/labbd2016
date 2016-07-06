d6340e02398c26d68cf4c5d79b9d5d1c5f6f5c34
jojima <fabio_jojima@hotmail.com>
2016-07-05 22:15:37 -0300

    relatorio funcionando
    agora a view do relatório está funcionando (já testado)

--- views/relatorio_calendario_administrativo_atv_adm.sql
+++ views/relatorio_calendario_administrativo_atv_adm.sql
@@ -4,20 +4,21 @@ CREATE
     SQL SECURITY DEFINER
 VIEW `calendario_adm_atividade_administrativas` AS
     SELECT 
-        `calendario`.`semestre` AS `semestre`,
-        `calendario`.`ano` AS `ano`,
-        `calendario`.`data_inicio` AS `inicio_calendario`,
-        `calendario`.`data_fim` AS `fim_calendario`,
-        `atividade_administrativa`.`nome` AS `nome`,
-        `atividade_administrativa`.`data_inicio` AS `atv_inicio`,
-        `atividade_administrativa`.`data_fim` AS `atv_fim`,
-		`atividade_administrativa`.`responsavel` AS `responsavel`
+        `labbd`.`calendario`.`semestre` AS `semestre`,
+        `labbd`.`calendario`.`ano` AS `ano`,
+        `labbd`.`calendario`.`data_inicio` AS `inicio_calendario`,
+        `labbd`.`calendario`.`data_fim` AS `fim_calendario`,
+        `labbd`.`atividade_administrativa`.`nome` AS `nome`,
+        `labbd`.`atividade_administrativa`.`data_inicio` AS `atv_inicio`,
+        `labbd`.`atividade_administrativa`.`data_fim` AS `atv_fim`,
+		`labbd`.`atividade_administrativa`.`responsavel` AS `responsavel`
     FROM
-        ((`administrativo`
-        JOIN (`calendario`
-        JOIN `possui_atividade_administrativa` `adm` ON ((`calendario`.`id` = `adm`.`id`))) ON ((`administrativo`.`id` = `calendario`.`id`)))
-        JOIN `atividade_administrativa` ON ((`atividade_administrativa`.`id_ativ_adm` = `adm`.`id_ativ_adm`)))
+        ((`labbd`.`calendario` 
+       JOIN (`labbd`.`atividade_administrativa`
+        JOIN `labbd`.`possui_atividade_administrativa` ON ((`labbd`.`atividade_administrativa`.`id_ativ_adm` = `labbd`.`possui_atividade_administrativa`.`id_ativ_adm`)))
+        JOIN  `labbd`.`administrativo` ON ((`labbd`.`administrativo`.`id` = `labbd`.`calendario`.`id`)) 
+        ))
 	WHERE
-		((`calendario`.`situacao` = 'aprovado') AND
-		 (`calendario`.`tipo` = 'administrativo'))
+		((`labbd`.`calendario`.`situacao` = 'aprovado') AND
+		 (`labbd`.`calendario`.`tipo` = 'administrativo'))
 	ORDER BY  atv_inicio;
