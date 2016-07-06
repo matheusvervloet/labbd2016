9e58b3435580804f9c588b79f0546e307370ad0a
brunowilliamsap <brunowilliamsap@gmail.com>
2016-07-06 00:40:09 -0300

    view resultado enade por curso ano periodo

---
+++ views/view_resultado_enade_curso.sql
@@ -0,0 +1,18 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `labbd`.`resultado_enade_curso` AS
+    SELECT 
+        `labbd`.`curso`.`nome` AS `Curso`,
+        `labbd`.`curso`.`sigla` AS `Sigla`,
+        `labbd`.`enade`.`ano` AS `Ano`,
+        `labbd`.`enade`.`periodo` AS `Periodo`,
+        `labbd`.`enade`.`resultado` AS `Resultado`,
+
+    FROM
+        (((`labbd`.`enade`
+        JOIN `labbd`.`curso`)
+    WHERE
+        ((`labbd`.`enade`.`sigla_curso` = `labbd`.`disciplina`.`sigla`))
+            
