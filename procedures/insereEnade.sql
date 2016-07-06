640c4448aaf200a70a75592218758921ff7b4b61
brunowilliamsap <brunowilliamsap@gmail.com>
2016-07-06 00:39:27 -0300

    adicionando resultado ao procedure

--- procedures/insereEnade.sql
+++ procedures/insereEnade.sql
@@ -1,8 +1,9 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insereEnade`(
         p_ano                 YEAR(4), 
         p_periodo             CHAR(10), 
-        p_sigla_curso         CHAR(10)
+        p_sigla_curso         CHAR(10),
+        p_resultado           CHAR(4),
 )
 BEGIN
-        INSERT INTO labbd.enade (ano, periodo, sigla_curso) VALUES  (ano, periodo, sigla_curso);
+        INSERT INTO labbd.enade (ano, periodo, sigla_curso, resultado) VALUES  (p_ano, p_periodo, sigla_curso, p_resultado);
 END
