eae33681b529377ef59330367feb388831f42fd0
brunowilliamsap <brunowilliamsap@gmail.com>
2016-07-06 05:42:46 -0300

    atualizando o procedure do enade

--- procedures/insereEnade.sql
+++ procedures/insereEnade.sql
@@ -2,8 +2,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `insereEnade`(
         p_ano                 YEAR(4), 
         p_periodo             CHAR(10), 
         p_sigla_curso         CHAR(10),
-        p_resultado           CHAR(4),
 )
 BEGIN
-        INSERT INTO labbd.enade (ano, periodo, sigla_curso, resultado) VALUES  (p_ano, p_periodo, sigla_curso, p_resultado);
+        INSERT INTO labbd.enade (ano, periodo, sigla_curso) VALUES  (p_ano, p_periodo, sigla_curso);
 END
