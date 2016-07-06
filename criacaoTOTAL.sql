6d7d42afc2cd8c8545c2a7acd7c504439e86f205
Raphael Adamski <raphaeladamski@hotmail.com>
2016-07-06 00:20:28 -0300

    Arrumando erro

--- criacaoTOTAL.sql
+++ criacaoTOTAL.sql
@@ -879,7 +879,7 @@ CREATE TRIGGER trigger_aluno_reenquadramento
     FOR EACH ROW
 BEGIN
     IF NEW.reenquadramento <> 0 THEN
-        UPDATE aluno SET perfil_atual = NEW.reenquadramento WHERE cpf = OLD.cpf
+        UPDATE aluno SET perfil_atual = NEW.reenquadramento WHERE cpf = OLD.cpf;
     END IF;
 END$$
 DELIMITER ;
