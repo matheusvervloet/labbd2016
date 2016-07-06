4993601d82cd3bff93084a44240dbd4eeaba8805
bbortolli <bbortolli2@hotmail.com>
2016-07-05 21:33:26 -0300

    correção de sintaxe

--- triggers/trigger-atualiza_status_matricula.sql
+++ triggers/trigger-atualiza_status_matricula.sql
@@ -6,15 +6,15 @@ ON matricula
 FOR EACH ROW
 begin
 	if new.frequencia < 75 AND new.nota < 6 THEN
-		set new.status = 'reprovado por nota e frequencia'
+		set new.status = 'reprovado por nota e frequencia';
 	else if new.frequencia >= 75 AND new.nota < 6 THEN
-		set new.status = 'reprovado por nota'
+		set new.status = 'reprovado por nota';
 	else if new.frequencia < 75 AND new.nota >= 6 THEN
-		set new.status = 'reprovado por frequencia'
+		set new.status = 'reprovado por frequencia';
 	else if new.frequencia >= 75 AND new.nota >= 6 THEN
-		set new.status = 'aprovado'
+		set new.status = 'aprovado';
 	else if new.frequencia = 0 AND new.nota = 6 THEN
-		set new.status = 'reconhecido'
+		set new.status = 'reconhecido';
 	end if;
 end$$ 
 DELIMITER ;
