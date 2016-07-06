749fb6c051c340e89030a180f24bf0bd1706cda3
matheusvervloet <matheusvervloet@users.noreply.github.com>
2016-07-06 04:31:37 -0300

    Algumas alterações

--- procedures/insereItens_de_pauta.sql
+++ procedures/insereItens_de_pauta.sql
@@ -1,4 +1,7 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insereItens_de_pauta`(npauta VARCHAR(255), ndata_aprovacao CHAR(10), nnumero_reuniao INT, ntexto_descritivo LONGTEXT)
+DELIMITER //
+DROP PROCEDURE IF EXISTS insereItens_de_pauta//
+CREATE DEFINER='root'@'localhost' PROCEDURE insereItens_de_pauta(npauta VARCHAR(255), ndata_aprovacao CHAR(10), nnumero_reuniao INT, ntexto_descritivo LONGTEXT)
 BEGIN
-	insert into labbd.itens_de_pauta(pauta , data_aprovacao, numero_reuniao,texto_descritivo) values (npauta, STR_TO_DATE(ndata_aprovacao, '%d-%m-%Y') , nnumero_reuniao ,ntexto_descritivo);
-END
+	INSERT INTO labbd.itens_de_pauta(pauta , data_aprovacao, numero_reuniao,texto_descritivo)
+	VALUES (npauta, STR_TO_DATE(ndata_aprovacao, '%Y-%m-%d') , nnumero_reuniao , ntexto_descritivo);
+END//
