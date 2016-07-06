da05540e03376e80a1dd845c152328a62d69cb17
bbortolli <bbortolli2@hotmail.com>
2016-07-05 21:50:05 -0300

    Update trigger-atualiza_status_matricula.sql

--- triggers/trigger-atualiza_status_matricula.sql
+++ triggers/trigger-atualiza_status_matricula.sql
@@ -6,13 +6,13 @@ CREATE TRIGGER t_atualiza_status_matricula
 BEGIN
 	if new.frequencia < 75 AND new.nota < 6 THEN
 		set new.status = 'reprovado por nota e frequencia';
-	else if new.frequencia >= 75 AND new.nota < 6 THEN
+	elseif new.frequencia >= 75 AND new.nota < 6 THEN
 		set new.status = 'reprovado por nota';
-	else if new.frequencia < 75 AND new.nota >= 6 THEN
+	elseif new.frequencia < 75 AND new.nota >= 6 THEN
 		set new.status = 'reprovado por frequencia';
-	else if new.frequencia >= 75 AND new.nota >= 6 THEN
+	elseif new.frequencia >= 75 AND new.nota >= 6 THEN
 		set new.status = 'aprovado';
-	else if new.frequencia = 0 AND new.nota = 6 THEN
+	elseif new.frequencia = 0 AND new.nota = 6 THEN
 		set new.status = 'reconhecido';
 	end if;
 END$$ 
