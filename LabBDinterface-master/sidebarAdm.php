c4d19ed0d62c8c2e0b9062419dd409a2707406b6
Matheus V <matheusvervloet@gmail.com>
2016-09-13 11:28:39 -0300

    atualizada barra lateral

--- LabBDinterface-master/sidebarAdm.php
+++ LabBDinterface-master/sidebarAdm.php
@@ -3,36 +3,39 @@
           <h2>Adm</h2>
           <ul><a href= http://localhost/my_site/Index.php>Sair</a></ul>
           <ul><a href= http://localhost/my_site/admIndex.php>Home</a></ul>
-          <h3>Cadastros</h3>
+          <h3>Tabelas</h3>
           <ul>
-			<li><a href="http://localhost/my_site/criarAluno.php">Aluno</a></li>
-			<li><a href="http://localhost/my_site/criaTA.php">Tecnico Administrativo</a></li>
-			<li><a href="http://localhost/my_site/criaDocente.php">Docente</a></li>
-            <li><a href="http://localhost/my_site/criarCentroAcademico.php">CentroAcademico</a></li>
-            <li><a href="http://localhost/my_site/criarDepartamento.php">Departamento</a></li>
-            <li><a href="http://localhost/my_site/criarDisciplina.php">Disciplina</a></li>
-            <li><a href="http://localhost/my_site/criarCurso.php">Curso</a></li>
-            <li><a href="http://localhost/my_site/criarReconhecimento.php">Reconhecimento</a></li>
-            <li><a href="http://localhost/my_site/criarAtividadeAdministrativa.php">AtividadeAdministrativa</a></li>
-            <li><a href="http://localhost/my_site/criarPossuiAtividadeAdministrativa.php">AtribuirAtvAdministrativa</a></li>
-            <li><a href="http://localhost/my_site/criarRecesso.php">Recesso</a></li>
-            <li><a href="http://localhost/my_site/criarPossuiRecesso.php">AtribuirRecesso</a></li>
+		<li><a href="http://localhost/my_site/criarAluno.php">Aluno</a></li>
+		<li><a href="http://localhost/my_site/criaTA.php">Tecnico Administrativo</a></li>
+		<li><a href="http://localhost/my_site/criaDocente.php">Docente</a></li>
+		<li><a href="http://localhost/my_site/criarCentroAcademico.php">Centro Academico</a></li>
+		<li><a href="http://localhost/my_site/criarFazProva.php">Provas feitas*</a></li>
+		<li><a href="http://localhost/my_site/criarMembro.php">Membro**</a></li>
+		<li><a href="http://localhost/my_site/criarReuniao.php">Reunião*</a></li>
+		<li><a href="http://localhost/my_site/criarItens.php">Itens de pauta</a></li>
+		<li><a href="http://localhost/my_site/criarDocumentos.php">Documentos*</a></li>
+		<li><a href="http://localhost/my_site/criarCurso.php">Curso</a></li>
+		<li><a href="http://localhost/my_site/criarDepartamento.php">Departamento</a></li>
+		<li><a href="http://localhost/my_site/criarDisciplina.php">Disciplina</a></li>
+		<li><a href="http://localhost/my_site/criarReconhecimento.php">Reconhecimento</a></li>
+		<li><a href="http://localhost/my_site/VerCalendarios.php">Calendario**</a></li>
+		<li><a href="http://localhost/my_site/criarRecesso.php">Recesso</a></li>
+		<li><a href="http://localhost/my_site/criarPossuiRecesso.php">Atribuir Recesso*</a></li>
+		<li><a href="http://localhost/my_site/criarAtividadeAdministrativa.php">Atividade Administrativa</a>
+		</li><li><a href="http://localhost/my_site/criarPossuiAtividadeAdministrativa.php">Atribuir Atividade Administrativa</a></li>
+		<li><a href="http://localhost/my_site/criarCursa.php">Cursa*</a></li>
+		<li><a href="http://localhost/my_site/criarItemDoFormulario.php">Item do formulário*</a></li>
+		<li><a href="http://localhost/my_site/criarVisita.php">Visita*</a></li>
+		<li><a href="http://localhost/my_site/criarComite.php">Comite*</a></li>
+		<li><a href="http://localhost/my_site/criarDecisoesAprovadas.php">Decisoes Aprovadas*</a></li>
+		<li><a href="http://localhost/my_site/criarTurma.php">Turma*</a></li>
+		<li><a href="http://localhost/my_site/criarEnade.php">Enade*</a></li>
+		
           </ul>
-          <h3>Açoes</h3>
+          <h3>Outros</h3>
           <ul>
-            <li><a href="http://localhost/my_site/fazDeferimento.php">Fazer Deferimento dos alunos</a></li>
-            <li><a href="http://localhost/my_site/VerCalendarios.php">Ver Calendarios</a></li>
-            <li><a href="http://localhost/my_site/verAtvAdm.php">Ver AtividadeAdministrativa</a></li>
-            <li><a href="http://localhost/my_site/procuraTA.php">Procurar TA</a></li>
-            <li><a href="http://localhost/my_site/procuraDocente.php">Procurar Docente</a></li>
+		<li><a href="http://localhost/my_site/fazDeferimento.php">Fazer Deferimento dos alunos</a></li>
           </ul>
           <h3>Left heading</h3>
-          <ul>
-            <li><a href="#">Link 1</a></li>
-            <li><a href="#">Link 2</a></li>
-            <li><a href="#">Link 3</a></li>
-            <li><a href="#">Link 4</a></li>
-            <li><a href="#">Link 5</a></li>
-          </ul>
         </div>
       </nav>
